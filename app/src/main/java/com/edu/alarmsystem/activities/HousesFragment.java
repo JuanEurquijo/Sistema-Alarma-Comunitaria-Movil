2765207559046c32e762493494174da7cedd41cb
JuanEurquijo <juancho.urquijo@gmail.com>
2023-03-08 23:16:30 -0500

    initial feature

---
+++ app/src/main/java/com/edu/alarmsystem/activities/HousesFragment.java
@@ -0,0 +1,22 @@
+package com.edu.alarmsystem.activities;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.edu.alarmsystem.R;
+
+
+public class HousesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_houses, container, false);
+    }
+}
